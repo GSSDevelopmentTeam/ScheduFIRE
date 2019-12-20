228186af17a5441e1872ed03cecc83fd27a2d138
cirocipo <ciro.cipo@live.it>
2019-12-20 16:33:33 +0100

    login e userDao

--- ScheduFIRE/src/model/dao/UserDao.java
+++ ScheduFIRE/src/model/dao/UserDao.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 
 
 
-import model.ConnessioneDB_Ciro;
+import model.ConnessioneDB;
 import model.bean.CredenzialiBean;
 
 public class UserDao {
@@ -19,55 +19,28 @@ public class UserDao {
 	public UserDao() {}
 
 
-	public CredenzialiBean login(String username, String password, String ruolo) {
+	public CredenzialiBean login(String username) {
 		CredenzialiBean credenziali = null;
 		try {
-			if(ruolo.equalsIgnoreCase("capoturno"))
-			{
-
-
-
-				Connection conn = ConnessioneDB_Ciro.getConnection();
-				PreparedStatement stm = conn.prepareStatement("SELECT * FROM credenziali WHERE username=? AND password=? AND ruolo='capoturno'");
-
+			
+				Connection conn = ConnessioneDB.getConnection();
+				PreparedStatement stm = conn.prepareStatement("SELECT * FROM credenziali WHERE username=?");
 				stm.setString(1, username);
-				stm.setString(2, password);
 				ResultSet res = stm.executeQuery();
 
 
 				credenziali = new CredenzialiBean();
 
-				//se il capoturno esiste
+				//se l'utente esiste
 				if(res.next()) {
 					credenziali.setUsername(res.getString(1));
 					credenziali.setPassword(res.getString(2));
-				}
-				else
-					return null;
-
-			}
-			else {
-				Connection conn = ConnessioneDB_Ciro.getConnection();
-				PreparedStatement stm = conn.prepareStatement("SELECT * FROM credenziali WHERE username=? AND password=? AND ruolo='vigiledelfuoco'");
-				stm.setString(1, username);
-				stm.setString(2, password);
-				ResultSet res = stm.executeQuery();
-
-
-				credenziali= new CredenzialiBean();
-
-				//se esiste il vigile del fuoco
-				if(res.next()) {
-					credenziali.setUsername(res.getString(1));
-					credenziali.setPassword(res.getString(2));
+					credenziali.setRuolo(res.getString(3));
 				}
 				else {
 					return null;
-				}
 
 			}
-
-
 
 		} catch (SQLException e) {
 
