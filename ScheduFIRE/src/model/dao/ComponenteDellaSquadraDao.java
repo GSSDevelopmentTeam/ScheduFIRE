28dc55336da3d7fd817bf74190e39341a7feba27
Emanuele Bombardelli <e.bombardelli97@gmail.com>
2019-12-24 16:06:17 +0100

    Aggiunto la query per salvare la lista dei componenti in ComponenteDellaSquadra.java

--- ScheduFIRE/src/model/dao/ComponenteDellaSquadraDao.java
+++ ScheduFIRE/src/model/dao/ComponenteDellaSquadraDao.java
@@ -6,12 +6,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+
 import model.ConnessioneDB;
 import model.bean.ComponenteDellaSquadraBean;
 
 public class ComponenteDellaSquadraDao {
 
-	
+
 	/**
 	 * @param data , la data del giorno di cui si vuole avere la lista dei vigili presenti nelle varie squadre
 	 * @return una lista di ComponenteDellaSquadraBean che sono nelle varie squadre della data passata come parametro
@@ -33,12 +35,12 @@ public class ComponenteDellaSquadraDao {
 				String email = rs.getString("emailVF");
 				String tipologia = rs.getString("tipologia");
 				Date giornoLavorativo = rs.getDate("giornoLavorativo");
-				
+
 				ComponenteDellaSquadraBean cds = new ComponenteDellaSquadraBean();
 				cds.setEmailVF(email);
 				cds.setTipologiaSquadra(tipologia);
 				cds.setGiornoLavorativo(giornoLavorativo);
-				
+
 				componenti.add(cds);
 			}
 			return componenti;
@@ -46,6 +48,28 @@ public class ComponenteDellaSquadraDao {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
+	public static boolean setComponenti(List<ComponenteDellaSquadraBean> componenti) {
+		try(Connection con = ConnessioneDB.getConnection()) {
+			int count = 0;
+			for(ComponenteDellaSquadraBean comp : componenti) {
+				count += aggiungiAlDb(comp, con);
+			}
+			return (count == componenti.size());
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static int aggiungiAlDb(ComponenteDellaSquadraBean comp, Connection con) throws SQLException {
+		PreparedStatement ps = con.prepareStatement("insert into Squadra(emailVF, tipologia, giornoLavorativo) "
+				+ "values (?, ?, ?);");
+		ps.setString(1, comp.getTipologiaSquadra());
+		ps.setString(2, comp.getEmailVF());
+		ps.setDate(3, comp.getGiornoLavorativo());
+
+		return ps.executeUpdate();
+
+	}
 
 }
