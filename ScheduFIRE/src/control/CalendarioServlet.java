7e22ea3e818f439c99bd7f11130da225ffc4c61f
francesca-perillo <f.perillo11@studenti.unisa.it>
2019-12-28 14:54:07 +0100

    modifiche calendario parte 2000

--- ScheduFIRE/src/control/CalendarioServlet.java
+++ ScheduFIRE/src/control/CalendarioServlet.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,6 +22,7 @@ import model.bean.VigileDelFuocoBean;
 import model.dao.ComponenteDellaSquadraDao;
 import model.dao.UserDao;
 import model.dao.VigileDelFuocoDao;
+import util.GiornoLavorativo;
 
 /**
  * Servlet implementation class CalendarioServlet
@@ -51,20 +55,21 @@ public class CalendarioServlet extends HttpServlet {
 		int giorno = Integer.parseInt(giorno_stringa_numero);
 		int mese = Integer.parseInt(mese_stringa_numero);
 		int anno = Integer.parseInt(anno_stringa_numero);
-		
-		String mese_stringa = month[mese-1];
 	
-	
-			
 		//array dei giorni del mese corrrente per riempire il calendario
 		int[] days_month= new int[42];
 		for(i=0;i<=41;i++) 
 			days_month[i]=-1;
 		
+		//array dei giorni lavorativi
+		int [] days_work = new int [42];
+		for(i=0; i<=41; i++)
+			days_work[i]=-1;
+	
+		
 		String meseJSP = request.getParameter("mese");
 		if(meseJSP!=null) {
 			mese = Integer.parseInt(meseJSP);
-			
 		}
 		
 		String annoJSP = request.getParameter("anno");
@@ -75,14 +80,13 @@ public class CalendarioServlet extends HttpServlet {
 	
 		//funzione per riempire il calendario
 		riempiCalendario(mese, anno, days_month);
-				
-		//println per controllo calendario 
-		System.out.println("Mese: ");
-		for(i=0; i<days_month.length;i++) {
-			System.out.println(" "+ days_month[i] + "; ");
-		}	
+		//funzione per i giorni lavorativi
+		riempiLavorativo(mese, anno, days_work);
 		
-		//per la schedulazione dei vigili
+		for(i=0; i<42; i++)
+			System.out.println(days_work[i]+"\n");
+		
+		//INIZIO schedulazione dei vigili
 		//creo i 4 array per ogni squadra
 		//conterranno i nomi dei vigili del fuoco
 		ArrayList<String> sala_operativa = new ArrayList<>();
@@ -115,40 +119,30 @@ public class CalendarioServlet extends HttpServlet {
 			switch (tipologia) {
 			case "Sala Operativa":
 				sala_operativa.add(cognome_nome);
-				System.out.println("Ho aggiunto un VF in sala operativa");
 				break;
 			case "Prima Partenza":
 				prima_partenza.add(cognome_nome);
-				System.out.println("Ho aggiunto un VF in prima partenza");
 				break;
 			case "Auto Scala":
 				autoscala.add(cognome_nome);
-				System.out.println("Ho aggiunto un VF in autoscala");
 				break;
 			case "Auto Botte":
 				autobotte.add(cognome_nome);
-				System.out.println("Ho aggiunto un VF in autobotte");
 				break;
 			default: 
 				//genera errore: nome squadra non consentito
-				System.out.println("CalendarioSERVLET -> nome squadra errato!");
 				break;
 			}
 		}
-		
-		System.out.println("sala operativa:"+sala_operativa.toString()+
-				"\nprima partenza"+prima_partenza.toString()+
-				"\nautobotte"+autobotte.toString()+
-				"\nautoscala"+autoscala.toString());
+		// FINE schedulazione vigili
 
-	
 		//attributi passati al CalendarioJSP.jsp
 		//array delle squadre
 		request.setAttribute("sala_operativa", sala_operativa);
 		request.setAttribute("prima_partenza", prima_partenza);
 		request.setAttribute("autoscala", autoscala);
 		request.setAttribute("autobotte", autobotte);
-		//...//
+		
 		request.setAttribute("anno_corrente", anno_stringa_numero);
 		request.setAttribute("mese_corrente", mese_stringa_numero);
 		request.setAttribute("anno", anno);
@@ -156,7 +150,7 @@ public class CalendarioServlet extends HttpServlet {
 		request.setAttribute("giorno", giorno);
 		request.setAttribute("meseStringa", month[mese-1]);
 		request.setAttribute("days_month", days_month);
-		
+		request.setAttribute("days_work", days_work);
 		
 		//dispatcher a calendario JSP
 		request.getRequestDispatcher("JSP/CalendarioJSP.jsp").forward(request, response); 
@@ -167,12 +161,7 @@ public class CalendarioServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
-	
-	/**
-	 * Funzione che specifica se l'anno è o no bisestile.
-	 * @param anno 
-	 * @return boolean specifica se l'anno sia o meno bisestile
-	 */
+
 	private boolean isBisestile(int anno) {
 		boolean bisestile = ( anno>1584 && 
 				( (anno%400==0) || 
@@ -182,27 +171,76 @@ public class CalendarioServlet extends HttpServlet {
 	}
 	
 	
-	/**
-	 * 
-	 * PRECONDIZIONE: array dei giorni della settinama gia inizializzato con valori -1. 
-	 * Questo metodo permette di modificare un array di interi 
-	 * passato come parametro alla funzione stessa.
-	 * 
-	 * Le posizioni dell'array contenenti valore pari a '-1'
-	 * sono posizioni considerate 'vuote' nel calendario.
-	 * 
-	 * @param mese mese dell'anno che si vuole riempire
-	 * @param day_first_week primo lunedi di quel mese
-	 * @param anno per la verifica del bisestile
-	 * @param days_month array da modificare, nel quale verranno inseriti i giorni corretti
-	 */
 	private void riempiCalendario (int mese, int anno, int[] days_month) {
 		
 			
-		//per vedere qual è il primo lunedì del mese
+		//per vedere qual è il primo giorno del mese
 		LocalDate local=LocalDate.of(anno,mese,1);
 	    String primoGiorno = local.getDayOfWeek().toString();
-		System.out.println("PRIMO GIORNO -> "+ primoGiorno);
+		
+		int day=-1;
+		
+		switch (primoGiorno) {
+
+		case "MONDAY": day = 0; break; //lunedì
+		case "TUESDAY": day = 1; break; //martedì
+		case "WEDNESDAY": day = 2; break; //mercoledì
+		case "THURSDAY": day = 3; break; //giovedì
+		case "FRIDAY": day = 4; break; //venerdì
+		case "SATURDAY": day = 5; break; //sabato
+		case "SUNDAY": day = 6; break; //domenica
+		default: break;
+		}
+		
+		int i;
+		
+		int giorno =1;
+		switch (mese) {
+		case 2: //mese febbraio
+							
+			if(isBisestile(anno)) { //anno bisestile
+				for(i=day;i<=28+day;i++) {
+					days_month[i] = giorno;
+					giorno++;
+				}
+			} else {
+				for(i=day;i<=27+day;i++) {
+					days_month[i] = giorno;
+					giorno++;
+				}
+			}			
+		break;
+		
+	case 11: case 4: case 6: case 9: //mesi di 30 giorni
+			
+			for (i=day; i<=29+day; i++) {
+				days_month[i] = giorno;
+				giorno ++;
+			}
+	
+		break;
+		
+	default: // mesi di 31 giorni
+	
+			for (i=day; i<=30+day; i++) {
+				days_month[i] = giorno;
+				giorno++;
+			}
+			
+		break;
+	}
+	
+}
+	/*
+	 * -1 -> giorno non da calendario
+	 * 1 -> giorno lavorativo diurno
+	 * 2 -> giorno lavorativo notturno
+	 */
+	private void riempiLavorativo(int mese, int anno, int[] days_work) {
+		
+		//per vedere qual è il primo giorno del mese
+		LocalDate local=LocalDate.of(anno,mese,1);
+	    String primoGiorno = local.getDayOfWeek().toString();
 		
 		int day=-1;
 		
@@ -220,43 +258,85 @@ public class CalendarioServlet extends HttpServlet {
 		
 		int i;
 	
+		int day_bis = day;
+		String data_stringa = "";
+		Date data;
+		
+		
 		int giorno =1;
-		System.out.println("SERVLET CALENDARIO: "+giorno);
 		switch (mese) {
 		case 2: //mese febbraio
 							
-				if(isBisestile(anno)) { //anno bisestile
-					for(i=day;i<=28+day;i++) {
-						days_month[i] = giorno;
-						giorno++;
-					}
-				} else {
-					for(i=day;i<=27+day;i++) {
-						days_month[i] = giorno;
-						giorno++;
-					}
-				}			
-			break;
-			
-		case 11: case 4: case 6: case 9: //mesi di 30 giorni
-				
-				for (i=day; i<=29+day; i++) {
-					days_month[i] = giorno;
-					giorno ++;
-				}
-		
-			break;
-			
-		default: // mesi di 31 giorni
-		
-				for (i=day; i<=30+day; i++) {
-					days_month[i] = giorno;
+			if(isBisestile(anno)) { //anno bisestile
+				for(i=day;i<=28+day;i++) {
+					data_stringa= anno+"-"+mese+"-"+giorno;
 					giorno++;
+					data = Date.valueOf(data_stringa);
+					if(GiornoLavorativo.isLavorativo(data)) {
+						
+						if(GiornoLavorativo.isDiurno(data)) {
+							days_work[i] = 1;
+						}else {
+							days_work[i] = 2;
+						}	
+					}
 				}
-				
-			break;
-		}
+			} else {
+				for(i=day;i<=27+day;i++) {
+					data_stringa= anno+"-"+mese+"-"+giorno;
+					giorno++;
+					data = Date.valueOf(data_stringa);
+					if(GiornoLavorativo.isLavorativo(data)) {
+						
+						if(GiornoLavorativo.isDiurno(data)) {
+							days_work[i] = 1;
+						}else {
+							days_work[i] = 2;
+						}	
+					}
+				}
+			}			
+		break;
 		
+	case 11: case 4: case 6: case 9: //mesi di 30 giorni
+			
+			for (i=day; i<=29+day; i++) {
+				data_stringa= anno+"-"+mese+"-"+giorno;
+				giorno++;
+				data = Date.valueOf(data_stringa);
+				if(GiornoLavorativo.isLavorativo(data)) {
+					
+					if(GiornoLavorativo.isDiurno(data)) {
+						days_work[i] = 1;
+					}else {
+						days_work[i] = 2;
+					}	
+				}
+			}
+	
+		break;
+		
+	default: // mesi di 31 giorni
+	
+			for (i=day; i<=30+day; i++) {
+				data_stringa= anno+"-"+mese+"-"+giorno;
+				giorno++;
+				data = Date.valueOf(data_stringa);
+				if(GiornoLavorativo.isLavorativo(data)) {
+					
+					if(GiornoLavorativo.isDiurno(data)) {
+						days_work[i] = 1;
+					}else {
+						days_work[i] = 2;
+					}	
+				}	
+			}
+			
+		break;
 	}
 	
+	}
+	
+	
+
 }
