663e857ccf73ff8160f18f854ea84d45c055e0d6
cirocipo <ciro.cipo@live.it>
2019-12-30 18:04:38 +0100

    classe email

---
+++ ScheduFIRE/src/model/bean/EmailBean.java
@@ -0,0 +1,23 @@
+package model.bean;
+
+public class EmailBean {
+
+	//variabili d'istanza
+		String  email;
+
+		public EmailBean() {
+			this.email=email;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public String setEmail(String email) {
+			 return this.email = email;
+		}
+
+		
+
+
+}
