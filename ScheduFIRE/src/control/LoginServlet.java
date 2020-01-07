ed757227866c89d6ceda5e698e35f94b8c9a4ec0
cirocipo <ciro.cipo@live.it>
2020-01-07 11:39:04 +0100

    sessione timer

--- ScheduFIRE/src/control/LoginServlet.java
+++ ScheduFIRE/src/control/LoginServlet.java
@@ -86,7 +86,8 @@ public class LoginServlet extends HttpServlet {
 							//RequestDispatcher dispatcher = request.getRequestDispatcher("WebContent\\JSP\\LoginJSP.jsp");
 							//dispatcher.forward(request, response);
 						} else {
-							session.setMaxInactiveInterval(3600); // 1 ora							response.sendRedirect("CalendarioServlet");
+							session.setMaxInactiveInterval(3600); // 1 ora
+							response.sendRedirect("CalendarioServlet");
 							return;
 							//RequestDispatcher dispatcher = request.getRequestDispatcher("/CalendarioServlet");
 							//dispatcher.forward(request, response);
