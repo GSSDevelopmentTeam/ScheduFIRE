27fab8af5d7d617df9ad10bee67cfe27f86e1f37
Giusy Annunziata <50444228+GiusyAnn@users.noreply.github.com>
2020-01-12 00:00:30 +0100

    ModificaVFServeletTest
    ModificaVFServletTest

--- ScheduFIRE/src/control/ModificaVFServlet.java
+++ ScheduFIRE/src/control/ModificaVFServlet.java
@@ -33,7 +33,7 @@ public class ModificaVFServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//Controllo login
 		Util.isCapoTurno(request);
@@ -43,7 +43,7 @@ public class ModificaVFServlet extends HttpServlet {
 		
 		//Ottenimento parametro email dalla richiesta
 		String emailVecchia = request.getParameter("emailVecchia");
-
+		System.out.println("l'email è "+emailVecchia);
 		//Controllo email
 		if( ! Validazione.email(emailVecchia) )
 			throw new ParametroInvalidoException("Il parametro 'email' Ã¨ errato!");
@@ -65,6 +65,7 @@ public class ModificaVFServlet extends HttpServlet {
 		String giorniFerieAnnoCorrenteNuoviStringa = request.getParameter("giorniFerieAnnoCorrenteNuovi");;
 		String giorniFerieAnnoPrecedenteNuoviStringa = request.getParameter("giorniFerieAnnoPrecedenteNuovi");
 		String emailNuova = request.getParameter("emailNuova");
+		System.out.println("PARAMETRI PRESI:"+nomeNuovo+cognomeNuovo+mansioneNuova+gradoNuovo+giorniFerieAnnoCorrenteNuoviStringa+giorniFerieAnnoPrecedenteNuoviStringa);
 		
 		if(giorniFerieAnnoCorrenteNuoviStringa == null ||
 			"".equals(giorniFerieAnnoCorrenteNuoviStringa))
@@ -149,7 +150,7 @@ public class ModificaVFServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
