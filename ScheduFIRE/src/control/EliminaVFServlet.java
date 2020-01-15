8447e7289eda34415f82603e3e9de4cc109780c7
EugenioSottile <58630888+EugenioSottile@users.noreply.github.com>
2020-01-15 11:15:51 +0100

    Aggiunta controllo isComponente

--- ScheduFIRE/src/control/EliminaVFServlet.java
+++ ScheduFIRE/src/control/EliminaVFServlet.java
@@ -1,6 +1,9 @@
 package control;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.dao.ComponenteDellaSquadraDao;
 import model.dao.VigileDelFuocoDao;
 import util.Util;
 import util.Validazione;
@@ -42,11 +46,30 @@ public class EliminaVFServlet extends HttpServlet {
 		
 		//Ottenimento parametro email dalla richiesta
 		String email = request.getParameter("email");
+		
 		//Controllo email
 		if( ! Validazione.email(email) )
 			throw new ParametroInvalidoException("Il parametro 'email' è errato!");
 		
-		if( ! VigileDelFuocoDao.setAdoperabile(email + "@vigilfuoco.it", false))
+		email += "@vigilfuoco.it";
+		
+		//Controllo se il vf scelto è gia schedulato in delle squadre
+		LocalDate inizio = LocalDate.now();
+		LocalDate fine = inizio.plusDays(7);
+		boolean eliminabile = true;
+		while( inizio.compareTo(fine) != 0 ) {
+			
+			eliminabile = eliminabile && ! ComponenteDellaSquadraDao.isComponente(email, Date.valueOf(inizio) );
+			
+			inizio = inizio.plusDays(1);
+			
+		}
+				
+		if( ! eliminabile )
+			throw new GestionePersonaleException("Non puoi modificare un Vigile del Fuoco scelto per le squadre"
+					+ " dei prossimi giorni lavorativi! Sostiuiscilo prima.");
+		
+		if( ! VigileDelFuocoDao.setAdoperabile(email, false))
 			throw new GestionePersonaleException("La cancellazione del vigile del fuoco non è andata a buon fine!");
 		
 		session.setAttribute("risultato", "La cancellazione del Vigile del Fuoco è avvenuto con successo!");
